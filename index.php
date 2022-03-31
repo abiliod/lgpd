2c735aacb5beda2d7a4257d219de44d4c34fac4e
Abilio Dias <abiliobonito@gmail.com>
2022-03-31 19:28:12 -0300

    Barra de aceite LGPD

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	/**
+	 * created by: Abilio Dias
+	 * #user ("Abilio Dias Ferreira")
+	 */
+?>
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Title</title>
+</head>
+<body>
+<h1>Pagina Qualquer</h1>
+<p>Algum texto da pagina</p>
+
+<script src="lgpd.js"></script>
+
+</body>
+</html>
+
---
+++ lgpd.js
@@ -0,0 +1,53 @@
+let lgpdUrl = 'https://jsonplaceholder.typicode.com/post';
+let lgpdHtml =
+    '<div class="lgpd">\n' +
+    '    <div class="lgpd--left">\n' +
+    '        Nós utilizamos cookie para melhorar sua experiência.<br/>\n' +
+    '        Para verificar os cookies utilizados, leia nossa <a href="politica_de_privacidade">Politica de Privacidade</a>\n' +
+    '    </div>\n' +
+    '    <div class="lgpd--rigth">\n' +
+    '        <button class="lgpdButton">Ok</button>\n' +
+    '    </div>\n' +
+    '</div>\n' +
+    '<link rel="stylesheet" href="lgpd.css"/>';
+
+let lsContent = localStorage.getItem('lgpd');
+
+if(!lsContent){
+
+    document.body.innerHTML += lgpdHtml;
+
+    let lgpdArea = document.querySelector(".lgpd");
+    let lgpdButton = document.querySelector(".lgpdButton");
+
+    lgpdButton?.addEventListener('click', async()=>{
+        lgpdArea.remove();
+
+        let result = await fetch(lgpdUrl);
+        let json = await result.json();
+
+        if (!json.error != ''){
+            let id = 123; // json.id;
+
+            localStorage.setItem('lgpd',id,'teste de cookie');
+        }
+
+    });
+
+    // #######  executar no console ############
+    //      localStorage.removeItem('lgpd')
+    // #######  executar no console ############
+}
+else{
+    alert (lsContent);  //seu cookie lggpd listando o valor
+
+}
+
+
+
+
+
+
+
+
+
